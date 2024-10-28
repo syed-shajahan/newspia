659dbe3334fdddb9ac3eb1d90c399d624ac71afc
syed-shajahan <shajahn7shaju@gmail.com>
2024-10-28 19:44:50 +0530

    added dark mode

--- src/components/home/Article.tsx
+++ src/components/home/Article.tsx
@@ -10,7 +10,7 @@ const ArticleList: FC<ArticleListProps> = ({ datas }) => {
       {datas?.articles.map((article, index) => (
         <div
           key={index}
-          className="bg-white mb-5 break shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105"
+          className="bg-white dark:bg-[#101010] mb-5 break shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105"
         >
           {/* Image Section */}
           {/* <div className="relative w-full overflow-hidden">
@@ -22,7 +22,7 @@ const ArticleList: FC<ArticleListProps> = ({ datas }) => {
             <Link href={article.url} passHref className="font-bold text-lg text-blue-600 hover:underline">
               {article.title}
             </Link>
-            <p className="text-sm text-gray-700 mt-2">{article.description}</p>
+            <p className="text-sm text-gray-700 dark:text-[#fff] mt-2">{article.description}</p>
             <Link href={article.url} passHref>
               <button className="mt-4 px-4 py-2 bg-black text-white rounded transition duration-200 hover:bg-blue-700">
                 Read More
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -2,20 +2,28 @@
 import { FC } from 'react';
 import Link from 'next/link';
 import { SearchBar } from './SearchInput';
+import { ModeToggle } from '../ModeToggle';
 
 const Header: FC = () => {
   return (
-    <nav className="flex justify-between items-center  z-10 sticky top-0 bg-[#fff] py-4">
+    <nav className="flex justify-between items-center  z-10 sticky top-0  p-4 border-b backdrop-blur-[3px]">
       <Link href="/">
         <div className="relative w-[200px] h-[50px]">
           {/* <Image src="/img/news-u-logo.webp" alt="logo" fill className="object-contain" /> */}
 
           <Link href={'/'}>
-            <h1 className="text-[45px] text-[#000] ">NewsPia</h1>
+            <h1 className="text-[45px] text-[#000] dark:text-[#fff] ">NewsPia</h1>
           </Link>
         </div>
       </Link>
-      <SearchBar />
+
+      <div className="flex items-center">
+        <SearchBar />
+
+        <div className="ms-2">
+          <ModeToggle />
+        </div>
+      </div>
     </nav>
   );
 };
